f3bd340bba7a3ad58d73f348ad1c2761264ea44a
Bogdan Dadu <dadu.bogdan@gmail.com>
2019-03-18 20:11:09 +0200

    first commit

---
+++ intro.js
@@ -0,0 +1,68 @@
+function getWelcomeMsg() {
+    return "Welcome to my stite";
+}
+function getColor() {
+    var date = new Date();
+    console.log(date.getMinutes());
+    if (date.getMinutes() > 16) {
+        return "green";
+    }
+    return "red";
+}
+
+
+var wellcome = getWelcomeMsg();
+console.info(wellcome);
+
+
+var summaryElement = document.getElementById("summary");
+console.info("summaryElement", summaryElement);
+var color = getColor();
+summaryElement.style.color = "color";
+
+
+
+
+function hidePage(page) {
+    var el = document.getElementById(page);
+    el.style.display = 'none';
+}
+function showPage(page) {
+    document.getElementById(page).style.display = 'block';
+
+}
+
+
+
+
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    console.info(links);
+    for (var i = 0; i < links.length; i++) {
+
+        links[i].onclick = clickOnMenuItem;
+    }
+
+}
+function clickOnMenuItem() {
+   hideAllPage();
+    var pageId=this.getAttribute('data-page');
+    showPage(pageId);
+}
+initMenu();
+function hideAllPage(){
+    var page=document.querySelectorAll('.page');
+    for (var i=0; i< page.length; i++){
+        page[i].style.display='none';
+    }
+
+}
+
+function showSkills(){
+    var skills=['html','css','js'];
+    for( var i=0; i<skills.length; i++){
+        console.log(i+1+'.' +skills[i]);
+    }
+}
+showSkills();
+
